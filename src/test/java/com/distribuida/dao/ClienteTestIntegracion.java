93b21ca2ee73a778932ad60ce7218c4ac7324cec
1150536249 <aabad@tecnologicoismac.edu.ec>
2025-11-25 03:22:01 +0100

    clase

--- src/test/java/com/distribuida/dao/ClienteTestIntegracion.java
+++ src/test/java/com/distribuida/dao/ClienteTestIntegracion.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
 import java.util.Optional;
 
-@DataJdbcTest
+@DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional
 @Rollback(value = false)
