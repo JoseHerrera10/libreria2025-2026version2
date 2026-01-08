e206fe4ec55dc9af88ca0a3723e270a1cfb81acc
1150536249 <aabad@tecnologicoismac.edu.ec>
2026-01-08 03:15:57 +0100

    controller

---
+++ src/main/java/com/distribuida/controller/AutorController.java
@@ -0,0 +1,67 @@
+package com.distribuida.controller;
+
+import com.distribuida.model.Autor;
+import com.distribuida.model.Categoria;
+import com.distribuida.service.AutorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/autor")
+public class AutorController {
+
+    @Autowired
+    private AutorService autorService;
+
+    @GetMapping
+    public ResponseEntity<List<Autor>> findAll(){
+
+        List<Autor> autors = autorService.findAll();
+        return ResponseEntity.ok(autors);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Autor> findOne(@PathVariable int id){
+        Optional<Autor> autor = autorService.findOne(id);
+        if(autor == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(autor.orElse(null));
+    }
+
+    @PostMapping
+    public ResponseEntity<Autor> save(@RequestBody Autor autor){
+
+        Autor autorNuevo = autorService.save(autor);
+        return ResponseEntity.ok(autorNuevo);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Autor> update(@PathVariable int id, @RequestBody Autor autor){
+
+        Autor autorActualizado = autorService.uptade(id, autor);
+        if(autorActualizado == null){
+            return  ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(autorActualizado);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id){
+        autorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+
+}
